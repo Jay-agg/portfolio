71a65d5de3f44574cfbe0942749f53bd7401040b
Jayant Aggarwal <jadps2004@gmail.com>
2024-06-26 04:26:00 +0530

    metadata img

--- constants.js
+++ constants.js
@@ -15,7 +15,7 @@ export const METADATA = {
     "Folio",
   ].join(", "),
   image:
-    "https://res.cloudinary.com/dywdhyojt/image/upload/v1690572126/preview.png",
+    "https://drive.google.com/file/d/1tdhQKNcAJpG5mLMKWAsJaWJTNTCFX3Yr/view?usp=sharing",
   language: "English",
   themeColor: "#000000",
 };
